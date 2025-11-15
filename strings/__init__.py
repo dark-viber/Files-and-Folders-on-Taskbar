3b1dbfa6377a8f541d0e739894e8fedd2ce3cfec
Dark Viber <michal.hales7@gmail.com>
2025-11-15 11:14:56 +0100

    Make Slovak recognized

--- strings/__init__.py
+++ strings/__init__.py
@@ -5,5 +5,6 @@ def load_language(language: str):
     if language == "default": language = locale.windows_locale[ctypes.windll.kernel32.GetUserDefaultUILanguage()]
     
     match language:
+        case "sk_SK": import strings.sk_SK; lang = strings.sk_SK
         case "ro_RO": import strings.ro_RO; lang = strings.ro_RO
         case _: import strings.en_US; lang = strings.en_US
